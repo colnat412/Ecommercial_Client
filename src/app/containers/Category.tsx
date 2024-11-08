17f267b0f5090179e8315ba3754d46d2c257f7c2
colnat412 <nguyentanloc1108@gmail.com>
2024-11-08 21:55:19 +0700

    Auto stash before merge of "loc" and "origin/main"

---
+++ src/app/containers/Category.tsx
@@ -0,0 +1,14 @@
+import { Category } from "@/src/types/category";
+import { StyleSheet, View } from "react-native";
+
+const CategoryItem = ({ id, name, image, description }: Category) => {
+  return (
+    <View>
+      <View></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CategoryItem;
