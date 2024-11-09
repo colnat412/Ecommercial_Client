e0cc28daaead2f1e286bbe5a788e77532911d5ae
colnat412 <nguyentanloc1108@gmail.com>
2024-11-09 16:55:48 +0700

    feat: not done icon search

--- src/app/containers/HomePage.tsx
+++ src/app/containers/HomePage.tsx
@@ -3,12 +3,14 @@ import { HomePageHeader } from "../navigation/components";
 import Category from "./category/Category";
 import Banner from "./Banner";
 import Recommended from "./recommended/Recommended";
+import { Search } from "./Search";
 
 export const HomePage = () => {
   return (
     <View style={{ flex: 1 }}>
       <HomePageHeader />
       <ScrollView>
+        <Search />
         <Category />
         <Banner />
         <Banner />
--- src/app/containers/Search.tsx
+++ src/app/containers/Search.tsx
@@ -2,12 +2,21 @@ import { style } from "@/src/constants";
 import { View } from "react-native";
 import { Text, TextInput } from "react-native-paper";
 import { DismissKeyboardView } from "../components";
+import { Search as SearchIcon } from "../../assets/";
 
 export const Search = () => {
   return (
     <DismissKeyboardView>
       <View style={[style.container]}>
-        <TextInput placeholder="Search" mode="outlined" />
+        <TextInput
+          left={
+            <View style={{ backgroundColor: "red", flex: 1 }}>
+              <SearchIcon width={20} color={"black"} />
+            </View>
+          }
+          placeholder="Search"
+          mode="outlined"
+        />
       </View>
     </DismissKeyboardView>
   );
