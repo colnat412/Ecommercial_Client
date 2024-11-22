ebe99f7e557f6b64638308056dce43fc7415c160
colnat412 <nguyentanloc1108@gmail.com>
2024-11-22 10:37:02 +0700

    resolve stash: auto STASH before merge of "loc" and "origin/minh"

--- src/app/containers/HomePage.tsx
+++ src/app/containers/HomePage.tsx
@@ -14,33 +14,9 @@ import { setDetailInfomation } from '@/src/libs/redux/store';
 
 export const HomePage = () => {
 	const [data, setData] = useState<Product[]>([]);
-	// useEffect(() => {
-	// 	getData({ urlApi: '/products' }).then((data) => {
-	// 		setData(data);
-	// 	});
-	// }, []);
-
-	// const dispatch = useAppDispatch();
-
-	// useEffect(() => {
-	// 	const fetchHomePage = async () => {
-	// 		const responseDetailInfomation = await fetchDetailInformation();
-	// 		if (responseDetailInfomation.status === 200) {
-	// 			dispatch(
-	// 				setDetailInfomation(
-	// 					responseDetailInfomation.data
-	// 						? responseDetailInfomation.data
-	// 						: null,
-	// 				),
-	// 			);
-	// 		} else if (responseDetailInfomation.status === 500) {
-	// 			console.log('Error');
-	// 		}
-	// 	};
-	// 	fetchHomePage();
-	// }, []);
 	useEffect(() => {
-		getDataFromDBS({ urlApi: '/api/products' }).then((data) => {
+		const res = getDataFromDBS({ urlApi: '/api/products' });
+		res.then((data) => {
 			setData(data.data);
 		});
 	}, []);
